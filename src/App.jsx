2223289d1f65e6fae2790bd4468fedc85fb5549a
Rodrigo Leon <leonrrodrigo@hotmail.com>
2023-08-14 09:05:01 -0600

    Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar.jsx';
+import Home from './components/Home.jsx';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Home />
     </>
   );
 }
