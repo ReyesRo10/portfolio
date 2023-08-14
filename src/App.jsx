14041cf8b3463694a571331b928c6bc04ef5400d
Rodrigo Leon <leonrrodrigo@hotmail.com>
2023-08-14 09:39:17 -0600

    Social links component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar.jsx';
 import Home from './components/Home.jsx';
+import SocialLinks from './components/SocialLinks.jsx';
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <SocialLinks />
     </>
   );
 }
